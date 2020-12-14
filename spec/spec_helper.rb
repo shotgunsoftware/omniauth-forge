d2b0a5102f15bb9a4f5f9b5a8be6c50df11ee9cc
Denis Zaratan Pasin <zaratan@hey.com>
2020-12-14 15:39:56 +0900

    No tests for now

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require "bundler/setup"
-require "omniauth/forge"
+require "omniauth-forge"
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure

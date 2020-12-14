603f7fd84735918dbe83a7762305521bb1d6aaef
Denis Zaratan Pasin <zaratan@hey.com>
2020-12-14 09:33:26 +0900

    INIT

---
+++ lib/omniauth/forge.rb
@@ -0,0 +1,8 @@
+require "omniauth/forge/version"
+
+module Omniauth
+  module Forge
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/omniauth/forge_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Omniauth::Forge do
+  it "has a version number" do
+    expect(Omniauth::Forge::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
